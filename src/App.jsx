e48b0300a15a29476e64672e910d02007f76868b
Akshay Jain <akshayjain2823@gmail.com>
2024-03-02 23:05:29 +0530

    File structure and routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Outlet } from "react-router-dom";
+import {
+	Header
+} from "./components/index";
+
 export default function App() {
 	return (
 		<>
-			<h1 className='text-3xl font-bold underline'>Hello world!</h1>
+			<div className="flex flex-col h-full">
+				<Header />
+				<Outlet/>
+			</div>
 		</>
 	);
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,45 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ContextProvider } from "./context/contextApi.jsx";
+import {
+	Route,
+	RouterProvider,
+	createBrowserRouter,
+	createRoutesFromElements,
+} from "react-router-dom";
+import Feed from "./components/Feed.jsx";
+import SearchResult from "./components/SearchResult.jsx";
+import VideoDetails from "./components/VideoDetails.jsx";
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route
+			path='/'
+			element={<App />}
+		>
+			<Route
+				path='/'
+				exact
+				element={<Feed />}
+			/>
+			<Route
+				path='/searchResult/:searchQuery'
+				element={<SearchResult />}
+			/>
+			<Route
+				path='/video/:id'
+				element={<VideoDetails />}
+			/>
+		</Route>
+	)
+);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<ContextProvider>
-			<App />
+			<RouterProvider router={router}>
+				<App />
+			</RouterProvider>
 		</ContextProvider>
 	</React.StrictMode>
 );
