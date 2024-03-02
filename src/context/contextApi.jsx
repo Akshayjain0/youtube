73b446c00be01cb4a2f030f267bf9a2e419537a4
Akshay Jain <akshayjain2823@gmail.com>
2024-03-02 13:14:45 +0530

    Api function and context created

---
+++ src/context/contextApi.jsx
@@ -0,0 +1,39 @@
+import { createContext, useState, useEffect } from "react";
+import { fetchDataFromApi } from "../utils/api";
+
+export const Context = createContext();
+
+export const ContextProvider = ({ children }) => {
+	const [loading, setLoading] = useState(false);
+	const [searchResult, setSearchResult] = useState(false);
+	const [selectCategory, setSelectCategory] = useState("New");
+    const [mobileMenu, setMobileMenu] = useState(false);
+    
+    useEffect(() => {
+        fetchSelectedCategoryData(selectCategory)
+    }, [selectCategory])
+    const fetchSelectedCategoryData = (query) => {
+        setLoading(true);
+        fetchDataFromApi(`search/?q=${query}`).then((contents) => {
+			console.log(contents);
+			setSearchResult(contents)
+            setLoading(false)
+        })
+    }
+	return (
+		<Context.Provider
+			value={{
+				loading,
+				setLoading,
+				searchResult,
+				setSearchResult,
+				selectCategory,
+				setSelectCategory,
+				mobileMenu,
+				setMobileMenu,
+			}}
+		>
+			{children}
+		</Context.Provider>
+	);
+};
